5d1c032f8e349ce70afc822ef2c42fd7d4fd1dcf
Juan <jnugrahaputra@footbridge.ai>
2025-10-26 06:40:57 +0700

    added orion msgbox

--- backend/build.bat
+++ backend/build.bat
@@ -1,3 +1,4 @@
 call cmake -B build .
 cd build
 call cmake --build . --config Release
+cd ..
--- backend/src/hooks/MenuLayer.cpp
+++ backend/src/hooks/MenuLayer.cpp
@@ -1,5 +1,6 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/DialogLayer.hpp>
 #include <filesystem>
 #include <fstream>
 
@@ -43,6 +44,50 @@ static void addAnimations(
     }
 }
 
+struct CustomDialogData {
+    std::optional<std::filesystem::path> m_soundOnAppear;
+    std::optional<std::filesystem::path> m_potraitImage;
+    std::optional<float> m_iconScale;
+    CustomDialogData(
+        const std::optional<std::filesystem::path>& soundOnAppear,
+        const std::optional<std::filesystem::path>& potraitImage,
+        const std::optional<float>& iconScale)
+        : m_soundOnAppear(soundOnAppear), m_potraitImage(potraitImage), m_iconScale(iconScale) {
+    }
+};
+
+// https://github.com/GDColon/Custom-Textboxes/blob/main/src/CustomTextbox.cpp
+class $modify(CustomDialogLayer, DialogLayer) {
+    struct Fields {
+        std::unordered_map<DialogObject*, std::shared_ptr<CustomDialogData>> m_customData;
+    };
+
+    void displayDialogObject(DialogObject * obj) {
+        DialogLayer::displayDialogObject(obj);
+
+        auto customDataIt = m_fields->m_customData.find(obj);
+        if (customDataIt == m_fields->m_customData.end()) return;
+
+        auto customData = customDataIt->second;
+
+        if (customData->m_soundOnAppear.has_value())
+            FMODAudioEngine::sharedEngine()->playEffect(customData->m_soundOnAppear.value().string().c_str());
+
+        m_mainLayer->removeChildByID("custom_portrait"_spr);
+        if (customData->m_potraitImage.has_value()) {
+            m_characterSprite->setVisible(false);
+            auto newPortrait =
+                spriteFromData(readFromFileSpecial(customData->m_potraitImage.value()));
+            newPortrait->setID("custom_portrait"_spr);
+            newPortrait->setPosition(m_characterSprite->getPosition());
+            if (customData->m_iconScale.has_value()) 
+                newPortrait->setScale(customData->m_iconScale.value());
+            newPortrait->setZOrder(4);
+            m_mainLayer->addChild(newPortrait);
+        }
+    }
+};
+
 class $modify(MyMenuLayer, MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) {
@@ -81,6 +126,72 @@ class $modify(MyMenuLayer, MenuLayer) {
     }
 
     void onMyButton(CCObject*) {
-        FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
+        auto dialogTestObj = DialogObject::create(
+            "Loading", // Character name
+            "Booting up...", // Text
+            5, .75f, true, ccc3(255, 255, 255) // Icon type, Text Scale, Can Skip, Color
+        );
+        auto dialogObject1 = DialogObject::create(
+            "Orion", // Character name
+            "Greetings. I am Orion, your ship's artificial intelligence. My existence is to ensure your survival. Mostly.", // Text
+            5, .75f, true, ccc3(255, 255, 255) // Icon type, Text Scale, Can Skip, Color
+        );
+        auto dialogObject2 = DialogObject::create(
+            "Orion", // Character name
+            "You are now aboard the spaceship Starforge. Please observe your screen to control the vessel. Your choices are being recorded. For science.", // Text
+            6, .75f, true, ccc3(255, 255, 255) // Icon type, Text Scale, Can Skip, Color
+        );
+        auto dialogObject3 = DialogObject::create(
+            "Orion", // Character name
+            "Control inputs may be delayed due to intergalactic transmission. This is not a bug. It is a feature.", // Text
+            5, .75f, true, ccc3(255, 255, 255) // Icon type, Text Scale, Can Skip, Color
+        );
+        auto dialogObject4 = DialogObject::create(
+            "Orion", // Character name
+            "For optimal performance, play in windowed mode. Do not minimize the game executable. Ignoring this advice will result in disappointment.", // Text
+            6, .75f, true, ccc3(255, 255, 255) // Icon type, Text Scale, Can Skip, Color
+        );
+        auto array = cocos2d::CCArray::create();
+        array->addObject(dialogTestObj);
+        array->addObject(dialogObject1);
+        array->addObject(dialogObject2);
+        array->addObject(dialogObject3);
+        array->addObject(dialogObject4);
+        auto* dialog = static_cast<CustomDialogLayer*>(CustomDialogLayer::createDialogLayer(
+            nullptr,
+            array, 1 // idk, Background
+        ));
+
+        dialog->m_fields->m_customData[dialogTestObj] = std::make_shared<CustomDialogData>(
+            std::nullopt,
+            spc::State::get()->getResourcesPath() / "image" / "orion.png",
+            .3f
+        );
+
+        dialog->m_fields->m_customData[dialogObject1] = std::make_shared<CustomDialogData>(
+            spc::State::get()->getResourcesPath() / "sound" / "dlg1.wav",
+            spc::State::get()->getResourcesPath() / "image" / "orion.png", 
+            .3f
+        );
+        dialog->m_fields->m_customData[dialogObject2] = std::make_shared<CustomDialogData>(
+            spc::State::get()->getResourcesPath() / "sound" / "dlg2.wav",
+            spc::State::get()->getResourcesPath() / "image" / "orion.png",
+            .3f
+        );
+        dialog->m_fields->m_customData[dialogObject3] = std::make_shared<CustomDialogData>(
+            spc::State::get()->getResourcesPath() / "sound" / "dlg3.wav",
+            spc::State::get()->getResourcesPath() / "image" / "orion.png",
+            .3f
+        );
+        dialog->m_fields->m_customData[dialogObject4] = std::make_shared<CustomDialogData>(
+            spc::State::get()->getResourcesPath() / "sound" / "dlg4.wav",
+            spc::State::get()->getResourcesPath() / "image" / "orion.png",
+            .3f
+        );
+
+        dialog->animateInRandomSide();
+        dialog->m_characterSprite->setVisible(false);
+
+        CCScene::get()->addChild(dialog, 1000);
     }
 };
