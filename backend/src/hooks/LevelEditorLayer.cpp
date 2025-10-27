0901d757b2ceaa2a35186e43fbc5b1f13bb1c33e
Juan <jnugrahaputra@footbridge.ai>
2025-10-28 04:48:38 +0700

    release: actual v1.0.0-beta.1

--- backend/src/hooks/LevelEditorLayer.cpp
+++ backend/src/hooks/LevelEditorLayer.cpp
@@ -1,5 +1,6 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/LevelEditorLayer.hpp>
+#include <Geode/modify/EditorUI.hpp>
 #include "../spc_state.h"
 
 using namespace geode::prelude;
@@ -25,5 +26,32 @@ class $modify(LevelEditorLayer) {
         auto state = spc::State::get();
         geode::queueInMainThread([]() { spc::State::get()->m_levelStateUpdate = true; });
         state->m_server->send(state->getEventMessage("editor_remove_object"));
+    }
+};
+
+class $modify(EditorUI) {
+    void moveObject(GameObject * p0, cocos2d::CCPoint p1) {
+        EditorUI::moveObject(p0, p1);
+        auto state = spc::State::get();
+        geode::queueInMainThread([]() { spc::State::get()->m_levelStateUpdate = true; });
+        state->m_server->send(state->getEventMessage("editor_move_object"));
+    }
+    void onCopy(cocos2d::CCObject* p0) {
+        EditorUI::onCopy(p0);
+        auto state = spc::State::get();
+        geode::queueInMainThread([]() { spc::State::get()->m_levelStateUpdate = true; });
+        state->m_server->send(state->getEventMessage("editor_copy_object"));
+    }
+    void onPaste(cocos2d::CCObject* p0) {
+        EditorUI::onPaste(p0);
+        auto state = spc::State::get();
+        geode::queueInMainThread([]() { spc::State::get()->m_levelStateUpdate = true; });
+        state->m_server->send(state->getEventMessage("editor_paste_object"));
+    }
+    void doPasteObjects(bool p0) {
+        EditorUI::doPasteObjects(p0);
+        auto state = spc::State::get();
+        geode::queueInMainThread([]() { spc::State::get()->m_levelStateUpdate = true; });
+        state->m_server->send(state->getEventMessage("editor_do_paste_object"));
     }
 };
